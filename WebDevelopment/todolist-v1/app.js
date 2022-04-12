4907465ad567d24dc59d812ba250a04e21d31a4d
nhuthlh <67490369+nhuthlh@users.noreply.github.com>
2022-04-12 09:24:36 +0700

    Add more ejs pages

--- WebDevelopment/todolist-v1/app.js
+++ WebDevelopment/todolist-v1/app.js
@@ -1,43 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { request } = require("express");
 
 const app = express();
 
+var items = [];
+var workItems = []
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded("extended", true));
+app.use(express.static("public"));
+
 
 app.get("/", function (req, res) {
     var today = new Date();
     var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error");
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {day: day});
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
 })
+
+app.get("/work", function(req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    })
+})
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    console.log(req.body);
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
+
 
 app.listen(3000, function () {
     console.log("Server is listening on port 3000");
