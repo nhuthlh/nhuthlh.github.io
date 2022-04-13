17666b144e8fbd8e3e62df458bef4ef389f15c07
nhuthlh <67490369+nhuthlh@users.noreply.github.com>
2022-04-13 10:09:57 +0700

    implement all features

--- WebDevelopment/ejs-challenge/app.js
+++ WebDevelopment/ejs-challenge/app.js
@@ -4,11 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+// Load the full build.
+const _ = require('lodash');
+ 
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+const at = require('lodash/at');
+const curryN = require('lodash/fp/curryN');
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const postList = [];
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +26,8 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.render("home", {
-    homeStartingContent: homeStartingContent
+    homeStartingContent: homeStartingContent,
+    postList: postList
   });
 })
 
@@ -44,6 +53,21 @@ app.post("/compose", function(req, res) {
     title: req.body.postTitle,
     body: req.body.postBody
   }
+  postList.push(post);
+  res.redirect("/");
+})
+
+app.get("/posts/:postName", function(req, res) {
+  let requestedTitle = _.lowerCase(req.params.postName);
+  postList.forEach(function(post) {
+    let storedTitle = _.lowerCase(post.title);
+    if (requestedTitle === storedTitle) {
+      res.render("post", {
+        title: requestedTitle,
+        content: post.body
+      })
+    }
+  })
 })
 
 
